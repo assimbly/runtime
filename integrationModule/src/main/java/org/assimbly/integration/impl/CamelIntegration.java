99916568ec07c34c3cf3d182e31688dcdfb01b11
Bruno Gonçalves <brunovg@gmail.com>
2022-06-06 09:54:45 +0100

    Aggregate component migration

--- integrationModule/src/main/java/org/assimbly/integration/impl/CamelIntegration.java
+++ integrationModule/src/main/java/org/assimbly/integration/impl/CamelIntegration.java
@@ -42,6 +42,7 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import world.dovetail.aggregate.AggregateStrategy;
 import world.dovetail.xmltojson.CustomXmlJsonDataFormat;
 
 import javax.xml.xpath.XPathFactory;
@@ -158,6 +159,7 @@ public class CamelIntegration extends BaseIntegration {
 		CustomHttpBinding customHttpBinding = new CustomHttpBinding();
 		registry.bind("customHttpBinding", customHttpBinding);
 		registry.bind("uuid-function", new UuidExtensionFunction());
+		registry.bind("CurrentAggregateStrategy", new AggregateStrategy());
 		context.addService(new CustomXmlJsonDataFormat());
 		//context.addComponent("my", new MyComponent(camelContext));
 		//End Dovetail specific beans
