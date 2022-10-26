13b836923cebca8241cfec168254f21f25e34f76
Bruno Gonçalves <brunovg@gmail.com>
2022-10-26 18:40:35 +0100

    changes on archive dil template

--- dil/src/main/java/org/assimbly/dil/blocks/templates/Archive.java
+++ dil/src/main/java/org/assimbly/dil/blocks/templates/Archive.java
@@ -7,24 +7,45 @@ public class Archive extends RouteBuilder {
      @Override
      public void configure() throws Exception {
 
-         routeTemplate("archive-action")
+         routeTemplate("zip-action")
+                 .templateParameter("routeconfiguration_id","0")
+                 .templateParameter("usingIterator", "true")
+                 .templateParameter("preservePathElements", "true")
+                 .templateParameter("in")
+                 .templateParameter("out")
+                 .from("{{in}}")
+                 .routeConfigurationId("{{routeconfiguration_id}}")
+                 .to("dataformat:zipFileDataFormat:marshal?usingIterator={{usingIterator}}&amp;preservePathElements={{preservePathElements}}")
+                 .to("{{out}}");
+
+         routeTemplate("zip-sink")
+                 .templateParameter("routeconfiguration_id","0")
+                 .templateParameter("usingIterator", "true")
+                 .templateParameter("preservePathElements", "true")
+                 .templateParameter("in")
+                 .from("{{in}}")
+                 .routeConfigurationId("{{routeconfiguration_id}}")
+                 .split().simple("${body}").streaming(true)
+                 .to("dataformat:zipFileDataFormat:marshal?usingIterator={{usingIterator}}&amp;preservePathElements={{preservePathElements}}");
+
+         routeTemplate("unzip-action")
                  .templateParameter("routeconfiguration_id","0")
                  .templateParameter("usingIterator", "true")
                  .templateParameter("in")
                  .templateParameter("out")
                  .from("{{in}}")
-                     .routeConfigurationId("{{routeconfiguration_id}}")
-                     .to("dataformat:checkedZipFileDataFormat:marshal?usingIterator={{usingIterator}}")
-                     .to("{{out}}");
+                 .routeConfigurationId("{{routeconfiguration_id}}")
+                 .to("dataformat:checkedZipFileDataFormat:unmarshal?usingIterator={{usingIterator}}")
+                 .to("{{out}}");
 
-         routeTemplate("archive-sink")
+         routeTemplate("unzip-sink")
                  .templateParameter("routeconfiguration_id","0")
                  .templateParameter("usingIterator", "true")
                  .templateParameter("in")
                  .from("{{in}}")
-                     .routeConfigurationId("{{routeconfiguration_id}}")
-                         .split().simple("${body}").streaming(true)
-                 .to("dataformat:checkedZipFileDataFormat:marshal?usingIterator={{usingIterator}}");
+                 .routeConfigurationId("{{routeconfiguration_id}}")
+                 .split().simple("${body}").streaming(true)
+                 .to("dataformat:checkedZipFileDataFormat:unmarshal?usingIterator={{usingIterator}}");
 
     }
 
