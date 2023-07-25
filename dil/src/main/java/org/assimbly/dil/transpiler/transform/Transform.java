1fcefc9005597422b48ecb6cb4c1f124e7cc1b75
skin27 <raymondmeester@gmail.com>
2023-07-25 09:05:13 +0200

    add log to camel2to3 converter

--- dil/src/main/java/org/assimbly/dil/transpiler/transform/Transform.java
+++ dil/src/main/java/org/assimbly/dil/transpiler/transform/Transform.java
@@ -41,6 +41,7 @@ public final class Transform {
         map.put("\"velocity:generate\"","\"velocity:generate?allowTemplateFromHeader=true\"");
         map.put("xslt:","xslt-saxon:");
         map.put("jetty:http:","jetty-nossl:http:");
+        map.put("log:nl.kabisa.flux//","log:org.assimbly.runtime");
         map.put("&amp;saxon=true","");
         map.put("?saxon=true\"","");
         map.put("?saxon=true&amp;","?");
