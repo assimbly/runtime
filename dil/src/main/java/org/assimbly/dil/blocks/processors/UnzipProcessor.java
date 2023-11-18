c0037f0a3b064f4232f6da18e2215f32562036e8
skin27 <raymondmeester@gmail.com>
2023-11-18 16:24:06 +0100

    update unzip processor

--- dil/src/main/java/org/assimbly/dil/blocks/processors/UnzipProcessor.java
+++ dil/src/main/java/org/assimbly/dil/blocks/processors/UnzipProcessor.java
@@ -3,9 +3,13 @@ package org.assimbly.dil.blocks.processors;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
+import org.apache.commons.io.FilenameUtils;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -16,17 +20,37 @@ public class UnzipProcessor implements Processor {
         Message in = exchange.getIn();
         InputStream inputStream = in.getBody(InputStream.class);
         ArrayList<byte[]> unzipped = new ArrayList<>();
+        List<String> fileExtensions = Arrays.asList(
+                "txt", "csv", "conf", "cfg", "data", "docx", "edi", "edifact", "edf","log", "ini",
+                "md", "msg", "bat", "sh", "json", "rtf", "tsv", "xml", "html", "yaml"
+        );
+        boolean textFiles = true;
 
         ZipInputStream zipInputStream = new ZipInputStream(inputStream);
+
+        //Get every file from the zip file
         ZipEntry entry;
         while ((entry = zipInputStream.getNextEntry()) != null) {
+            String fileExtension = FilenameUtils.getExtension(entry.getName());
+            if (!fileExtensions.contains(fileExtension)) {
+                textFiles = false;
+            }
             if (!entry.isDirectory()) {
                 unzipped.add(getZipContents(zipInputStream));
             }
             zipInputStream.closeEntry();
         }
 
-        in.setBody(unzipped);
+        //If the zip file only contains text files than convert the files to utf-8 strings
+        if(textFiles){
+            ArrayList<String> unzippedText = new ArrayList<>();
+            for (byte[] byteArray : unzipped) {
+                unzippedText.add(new String(byteArray, StandardCharsets.UTF_8));
+            }
+            in.setBody(unzippedText);
+        } else{
+            in.setBody(unzipped);
+        }
 
     }
 
