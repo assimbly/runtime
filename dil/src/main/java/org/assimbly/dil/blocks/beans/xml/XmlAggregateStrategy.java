79c5bbfd366ba99bfd6feaee7f82237cca1c91bf
Raymond Meester <raymondmeester@gmail.com>
2024-04-03 18:30:04 +0200

    update AggregateStrategy

--- dil/src/main/java/org/assimbly/dil/blocks/beans/xml/XmlAggregateStrategy.java
+++ dil/src/main/java/org/assimbly/dil/blocks/beans/xml/XmlAggregateStrategy.java
@@ -10,6 +10,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
+import org.w3c.dom.bootstrap.DOMImplementationRegistry;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSSerializer;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -17,7 +21,11 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
+import java.util.Map;
+
+import static org.apache.camel.builder.Builder.simple;
 
 
 public class XmlAggregateStrategy implements AggregationStrategy {
@@ -29,27 +37,26 @@ public class XmlAggregateStrategy implements AggregationStrategy {
 
         try {
 
-            Document aggregated = DocConverter.convertStringToDoc("<Aggregated/>");
+            boolean CamelSplitComplete = newExchange.getProperty("CamelSplitComplete",Boolean.class);
 
-            Document originalXml = getXml(oldExchange),
-                    resourceXml = getXml(newExchange);
+            String newXml = getXml(oldExchange, "old"),
+                    splitXml = getXml(newExchange, "new");
 
-            if(originalXml == null && resourceXml == null) {
+            if(newXml == null && splitXml == null) {
                 throw new Exception("XML Aggregate: Something went wrong parsing the XML inputs.");
-            }else if(originalXml == null) {
-                aggregated = mergeDoc(aggregated, resourceXml);
-                newExchange.getIn().setBody(aggregated);
+            }else if(newXml==null){
+                newExchange.getIn().setBody(splitXml);
                 return newExchange;
-            }else if (resourceXml == null) {
-                aggregated = mergeDoc(aggregated, originalXml);
-            }else if(originalXml.getDocumentElement().getTagName().equals("Aggregated")){
-                aggregated = mergeDoc(originalXml, resourceXml);
             }else{
-                aggregated = mergeDoc(aggregated, originalXml);
-                aggregated = mergeDoc(aggregated, resourceXml);
+                newXml = newXml + splitXml;
             }
 
-            oldExchange.getIn().setBody(aggregated);
+            if(CamelSplitComplete){
+                newXml = format("<Aggregated>" + newXml + "</Aggregated>");
+                oldExchange.getIn().setBody(newXml);
+            }else{
+                oldExchange.getIn().setBody(newXml);
+            }
 
         } catch (Exception e) {
             log.error(e.getMessage(), e);
@@ -57,18 +64,10 @@ public class XmlAggregateStrategy implements AggregationStrategy {
         return oldExchange;
     }
 
-    private Document getXml(Exchange exchange) {
+    private String getXml(Exchange exchange, String x) {
 
         try {
-            String xml = exchange.getIn().getBody(String.class);
-            Document document = DocConverter.convertStringToDoc(xml);
-
-            if (document == null) {
-                log.warn("No valid XML returned by the route to the Aggregate component.");
-            }
-
-            return document;
-
+            return exchange.getIn().getBody(String.class);
         } catch (Exception e) {
 
             if (log.isDebugEnabled()) {
@@ -79,12 +78,27 @@ public class XmlAggregateStrategy implements AggregationStrategy {
         return null;
     }
 
-    public static Document mergeDoc(Document original, Document addition) {
-        Node copy = original.importNode(addition.getFirstChild(), true);
-        original.getFirstChild().appendChild(copy);
-        return original;
+    public String format(String xml) {
+
+        try {
+            final InputSource src = new InputSource(new StringReader(xml));
+            final Node document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(src).getDocumentElement();
+            final Boolean keepDeclaration = Boolean.valueOf(xml.startsWith("<?xml"));
+
+            //May need this: System.setProperty(DOMImplementationRegistry.PROPERTY,"com.sun.org.apache.xerces.internal.dom.DOMImplementationSourceImpl");
+
+
+            final DOMImplementationRegistry registry = DOMImplementationRegistry.newInstance();
+            final DOMImplementationLS impl = (DOMImplementationLS) registry.getDOMImplementation("LS");
+            final LSSerializer writer = impl.createLSSerializer();
+
+            writer.getDomConfig().setParameter("format-pretty-print", Boolean.TRUE); // Set this to true if the output needs to be beautified.
+            writer.getDomConfig().setParameter("xml-declaration", keepDeclaration); // Set this to true if the declaration is needed to be outputted.
+
+            return writer.writeToString(document);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
-
-
 
 }
