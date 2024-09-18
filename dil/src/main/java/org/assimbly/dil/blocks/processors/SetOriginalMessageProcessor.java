919be666550ee69b8eb4c37e31634326fa73b308
Bruno Gonçalves <brunovg@gmail.com>
2024-09-18 11:58:06 +0100

    saving original http message into camel global variable

--- dil/src/main/java/org/assimbly/dil/blocks/processors/SetOriginalMessageProcessor.java
+++ dil/src/main/java/org/assimbly/dil/blocks/processors/SetOriginalMessageProcessor.java
@@ -3,42 +3,79 @@ package org.assimbly.dil.blocks.processors;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Map;
-
-//set headers for each step
 public class SetOriginalMessageProcessor implements Processor {
 
-	private static String DOVETAIL_SET_ORIGINAL_HTTP_MESSAGE_PROP = "DOVETAIL_setOriginalHttpMessage";
-	private static String DOVETAIL_ORIGINAL_HTTP_MESSAGE_PROP = "DOVETAIL_originalHttpMessage";
+	private static String DOVETAIL_ORIGINAL_HTTP_MESSAGE_METHOD_PROP = "DOVETAIL_originalHttpMessageMethod";
 
-	private static String DOVETAIL_RETRY_ATTEMPTS_HEAD = "DOVETAIL_RetryAttempts";
+	private static String DOVETAIL_RETRY_ATTEMPTS_HEADER = "DOVETAIL_RetryAttempts";
+	private static String DOVETAIL_ORIGINAL_HTTP_MESSAGE_VARIABLE_ID_HEADER = "DOVETAIL_OriginalHttpMessageVariableId";
+
+	private static String CAMEL_GLOBAL_VARIABLE_PREFIX = "global:";
+
+	public enum HttpMethod {GET, SET, DEL}
+
+	protected Logger log = LoggerFactory.getLogger(getClass());
 
 	public void process(Exchange exchange) throws Exception {
-		Object setOriginalHttpMessage = exchange.getProperty(DOVETAIL_SET_ORIGINAL_HTTP_MESSAGE_PROP);
+		String variableId;
 
-		if(setOriginalHttpMessage!=null) {
-			// first removes DOVETAIL_setOriginalHttpMessage
-			exchange.removeProperty(DOVETAIL_SET_ORIGINAL_HTTP_MESSAGE_PROP);
-			// set DOVETAIL_ORIGINAL_HTTP_MESSAGE_PROP
-			exchange.setProperty(DOVETAIL_ORIGINAL_HTTP_MESSAGE_PROP, exchange.getMessage().copy());
+		String methodStr = (String) exchange.getProperty(DOVETAIL_ORIGINAL_HTTP_MESSAGE_METHOD_PROP);
+		HttpMethod originalHttpMessageMethod = getHttpMethod(methodStr);
 
-		} else {
-			// load DOVETAIL_ORIGINAL_HTTP_MESSAGE_PROP
-			Message originalHttpMessage = (Message)exchange.getProperty(DOVETAIL_ORIGINAL_HTTP_MESSAGE_PROP);
-			// load retry attempts
-			Object retryAttempts = originalHttpMessage.getHeader(DOVETAIL_RETRY_ATTEMPTS_HEAD);
+		switch (originalHttpMessageMethod) {
+			case GET:
+				// load the original http message into the current exchange from the camel global variable
 
-			if(retryAttempts==null){
-				originalHttpMessage.setHeader(DOVETAIL_RETRY_ATTEMPTS_HEAD,1);
-			}else{
-				originalHttpMessage.setHeader(DOVETAIL_RETRY_ATTEMPTS_HEAD,(Integer)retryAttempts + 1);
-			}
-			// set updated DOVETAIL_ORIGINAL_HTTP_MESSAGE_PROP
-			exchange.setProperty(DOVETAIL_ORIGINAL_HTTP_MESSAGE_PROP, originalHttpMessage);
+				// load retry attempts
+				Object retryAttempts = exchange.getMessage().getHeader(DOVETAIL_RETRY_ATTEMPTS_HEADER);
+				int retryAttemptsInt = (Integer)retryAttempts + 1;
 
-			// load original http message into exchange
-			exchange.setMessage(originalHttpMessage);
+				// load original http message variable id
+				variableId = (String) exchange.getMessage().getHeader(DOVETAIL_ORIGINAL_HTTP_MESSAGE_VARIABLE_ID_HEADER);
+
+				// load original http message from global variable
+				Message originalHttpMessage = (Message) exchange.getVariable(CAMEL_GLOBAL_VARIABLE_PREFIX + variableId);
+
+				// set retry attempts
+				originalHttpMessage.setHeader(DOVETAIL_RETRY_ATTEMPTS_HEADER, retryAttemptsInt);
+
+				// load original http message into exchange
+				exchange.setMessage(originalHttpMessage);
+				break;
+
+			case SET:
+				// set the current exchange message as the original http message in the camel global variable
+
+				// set retry attempts to 1
+				exchange.getMessage().setHeader(DOVETAIL_RETRY_ATTEMPTS_HEADER, 0);
+
+				// using exchangeId as http message variable id
+				String exchangeId = exchange.getExchangeId();
+				// set original http message variable id
+				exchange.getMessage().setHeader(DOVETAIL_ORIGINAL_HTTP_MESSAGE_VARIABLE_ID_HEADER, exchangeId);
+
+				// set original http message on a global variable
+				exchange.setVariable(CAMEL_GLOBAL_VARIABLE_PREFIX + exchangeId, exchange.getMessage().copy());
+				break;
+
+			case DEL:
+				// delete the original http message from the camel global variables
+				variableId = (String) exchange.getMessage().getHeader(DOVETAIL_ORIGINAL_HTTP_MESSAGE_VARIABLE_ID_HEADER);
+				exchange.removeVariable(CAMEL_GLOBAL_VARIABLE_PREFIX + variableId);
+				break;
+
+		}
+	}
+
+	private HttpMethod getHttpMethod(String methodStr) {
+		try {
+			return HttpMethod.valueOf(methodStr);
+		} catch (IllegalArgumentException | NullPointerException e) {
+			log.error("Invalid HTTP method: " + methodStr + ". Using default: " + HttpMethod.GET);
+			return HttpMethod.GET;
 		}
 	}
 
