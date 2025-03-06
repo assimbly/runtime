adb4f7180ca28e39319eca1c2291e9da96dfab29
Bruno Gonçalves <brunovg@gmail.com>
2025-03-06 13:47:30 +0000

    defined collector, flow interaction, stats and others areas, inside Integration unit tests

--- integrationRest/src/test/java/org/assimbly/integrationrest/IntegrationRuntimeTest.java
+++ integrationRest/src/test/java/org/assimbly/integrationrest/IntegrationRuntimeTest.java
@@ -43,31 +43,12 @@ public class IntegrationRuntimeTest {
         }
     }
 
-    @Test
-    void shouldBeStarted() {
-        try {
-            // url
-            String baseUrl = AssimblyGatewayHeadlessContainer.getBaseUrl();
-            String url = baseUrl + "/api/integration/isstarted";
+    //////////////////////////////////////////
+    // collector
+    // TODO
 
-            // headers
-            HashMap<String, String> headers = new HashMap();
-            headers.put("Accept", MediaType.APPLICATION_JSON_VALUE);
-
-            // endpoint call - check if backend is started
-            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "GET", null, null, headers);
-
-            // asserts
-            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode expectedBodyJson = objectMapper.readTree("{\"path\":\"/integration/isstarted\",\"details\":\"successful\",\"id\":\"1\",\"message\":\"true\",\"status\":200}");
-            assertThatJson(response.body()).whenIgnoringPaths("timestamp").isEqualTo(expectedBodyJson);
-
-        } catch (Exception e) {
-            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
-        }
-    }
+    //////////////////////////////////////////
+    // flow interaction
 
     @Test
     void shouldInstallFlow() {
@@ -87,31 +68,6 @@ public class IntegrationRuntimeTest {
 
             // asserts
             assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
-
-        } catch (Exception e) {
-            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
-        }
-    }
-
-    @Test
-    void shouldGetEmptyFlowStats() {
-        try {
-            // url
-            String baseUrl = AssimblyGatewayHeadlessContainer.getBaseUrl();
-            String url = String.format("%s/api/integration/flow/%s/stats", baseUrl, inboundHttpsCamelContextProp.get(CamelContextUtil.Field.id.name()));
-
-            // headers
-            HashMap<String, String> headers = new HashMap();
-            headers.put("Accept", MediaType.APPLICATION_JSON_VALUE);
-            headers.put("charset", StandardCharsets.ISO_8859_1.displayName());
-            headers.put("Content-type", MediaType.APPLICATION_XML_VALUE);
-
-            // endpoint call - get flow stats
-            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "GET", (String) inboundHttpsCamelContextProp.get(CamelContextUtil.Field.camelContext.name()), null, headers);
-
-            // asserts
-            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
-            assertThatJson(response.body()).isEqualTo("{\"flow\":{\"total\":0,\"pending\":0,\"id\":\"67921474ecaafe0007000000\",\"completed\":0,\"failed\":0}}");
 
         } catch (Exception e) {
             fail("Test failed due to unexpected exception: " + e.getMessage(), e);
@@ -140,6 +96,63 @@ public class IntegrationRuntimeTest {
             ObjectMapper objectMapper = new ObjectMapper();
             JsonNode expectedBodyJson = objectMapper.readTree("{\"flow\":{\"isRunning\":true,\"name\":\"67c740bc349ced00070004a9\",\"id\":\"67c740bc349ced00070004a9\",\"status\":\"started\"}}");
             assertThatJson(response.body()).whenIgnoringPaths("flow.uptime").isEqualTo(expectedBodyJson);
+
+        } catch (Exception e) {
+            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
+        }
+    }
+
+    //////////////////////////////////////////
+    // stats
+
+    @Test
+    void shouldGetEmptyFlowStats() {
+        try {
+            // url
+            String baseUrl = AssimblyGatewayHeadlessContainer.getBaseUrl();
+            String url = String.format("%s/api/integration/flow/%s/stats", baseUrl, inboundHttpsCamelContextProp.get(CamelContextUtil.Field.id.name()));
+
+            // headers
+            HashMap<String, String> headers = new HashMap();
+            headers.put("Accept", MediaType.APPLICATION_JSON_VALUE);
+            headers.put("charset", StandardCharsets.ISO_8859_1.displayName());
+            headers.put("Content-type", MediaType.APPLICATION_XML_VALUE);
+
+            // endpoint call - get flow stats
+            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "GET", (String) inboundHttpsCamelContextProp.get(CamelContextUtil.Field.camelContext.name()), null, headers);
+
+            // asserts
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
+            assertThatJson(response.body()).isEqualTo("{\"flow\":{\"total\":0,\"pending\":0,\"id\":\"67921474ecaafe0007000000\",\"completed\":0,\"failed\":0}}");
+
+        } catch (Exception e) {
+            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
+        }
+    }
+
+    //////////////////////////////////////////
+    // others
+
+    @Test
+    void shouldBeStarted() {
+        try {
+            // url
+            String baseUrl = AssimblyGatewayHeadlessContainer.getBaseUrl();
+            String url = baseUrl + "/api/integration/isstarted";
+
+            // headers
+            HashMap<String, String> headers = new HashMap();
+            headers.put("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+            // endpoint call - check if backend is started
+            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "GET", null, null, headers);
+
+            // asserts
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode expectedBodyJson = objectMapper.readTree("{\"path\":\"/integration/isstarted\",\"details\":\"successful\",\"id\":\"1\",\"message\":\"true\",\"status\":200}");
+            assertThatJson(response.body()).whenIgnoringPaths("timestamp").isEqualTo(expectedBodyJson);
 
         } catch (Exception e) {
             fail("Test failed due to unexpected exception: " + e.getMessage(), e);
