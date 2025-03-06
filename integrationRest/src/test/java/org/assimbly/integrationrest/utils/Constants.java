0643ed78161b7a7af0c4a8c63a64bdd0b80480f9
Bruno Gonçalves <brunovg@gmail.com>
2025-03-06 11:25:41 +0000

    Constants class - constants to be used on unit tests

---
+++ integrationRest/src/test/java/org/assimbly/integrationrest/utils/Constants.java
@@ -0,0 +1,14 @@
+package org.assimbly.integrationrest.utils;
+
+public class Constants {
+
+    public static String DB = "mongo";
+    public static String DOMAIN_NAME = "test.assimbly.org";
+
+    // user related
+    public static String FIRST_NAME_USER = "Anne";
+    public static String LAST_NAME_USER = "Frank";
+    public static String PASSWORD_USER = "frankfurt1929";
+    public static String EMAIL_USER = "anne.frank@assimbly.com";
+
+}
