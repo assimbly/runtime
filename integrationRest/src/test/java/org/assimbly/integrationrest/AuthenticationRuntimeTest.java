eba0843729e816bc48063d92da6c7c8a5a160b47
Bruno Gonçalves <brunovg@gmail.com>
2025-03-06 11:32:22 +0000

    unit tests for Authentication endpoints

--- integrationRest/src/test/java/org/assimbly/integrationrest/AuthenticationRuntimeTest.java
+++ integrationRest/src/test/java/org/assimbly/integrationrest/AuthenticationRuntimeTest.java
@@ -1,57 +1,33 @@
 package org.assimbly.integrationrest;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.assimbly.integrationrest.testcontainers.AssimblyGatewayHeadlessContainer;
+import org.assimbly.integrationrest.utils.Constants;
+import org.assimbly.integrationrest.utils.GoogleTOTPUtil;
 import org.assimbly.integrationrest.utils.HttpUtil;
+import org.assimbly.integrationrest.utils.MongoUtil;
+import org.bson.Document;
 import org.eclipse.jetty.http.HttpStatus;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.http.MediaType;
 
+import java.net.URLDecoder;
 import java.net.http.HttpResponse;
+import java.util.Base64;
 import java.util.HashMap;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class AuthenticationRuntimeTest {
 
-    private static String ID_TOKEN = "id_token";
-
-    public static String getTokenId() {
-        try {
-            // url
-            String baseUrl = AssimblyGatewayHeadlessContainer.getBaseUrl();
-            String url = String.format("%s/api/authenticate", baseUrl);
-
-            // headers
-            HashMap<String, String> headers = new HashMap();
-            headers.put("Content-type", MediaType.APPLICATION_JSON_VALUE);
-            headers.put("db", "mongo");
-
-            // body
-            String body = "{\"username\": \"admin\", \"password\": \"admin\", \"rememberMe\": \"false\"}";
-
-            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "POST", body, null, headers);
-
-            if(response.statusCode() != HttpStatus.OK_200) {
-                return null;
-            }
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode bodyJson = objectMapper.readTree(response.body());
-
-            if(bodyJson.get(ID_TOKEN) != null) {
-                return bodyJson.get(ID_TOKEN).textValue();
-            }
-
-            return null;
-
-        } catch (Exception e) {
-            return null;
-        }
-    }
+    private static String authToken;
+    private static String totpSecret;
 
     @Test
     void shouldAuthenticateAndGetToken() {
@@ -63,15 +39,146 @@ public class AuthenticationRuntimeTest {
             // headers
             HashMap<String, String> headers = new HashMap();
             headers.put("Content-type", MediaType.APPLICATION_JSON_VALUE);
-            headers.put("db", "mongo");
+            headers.put("db", Constants.DB);
 
             // body
             String body = "{\"username\": \"admin\", \"password\": \"admin\", \"rememberMe\": \"false\"}";
 
+            // endpoint call
             HttpResponse<String> response = HttpUtil.makeHttpCall(url, "POST", body, null, headers);
 
+            // assertions
             assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
-            assertThatJson(response.body()).inPath(ID_TOKEN);
+            assertThatJson(response.body()).inPath("id_token");
+
+        } catch (Exception e) {
+            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
+        }
+    }
+
+    @Test
+    @Order(1)
+    void shouldAuthenticateAndGenerateDBToken() {
+        try {
+            // create user on mongodb
+            MongoUtil.createUser(Constants.FIRST_NAME_USER, Constants.LAST_NAME_USER, Constants.EMAIL_USER, Constants.PASSWORD_USER);
+
+            // url
+            String baseUrl = AssimblyGatewayHeadlessContainer.getBaseUrl();
+            String url = String.format("%s/api/db/authenticate", baseUrl);
+
+            // headers
+            HashMap<String, String> headers = new HashMap();
+            String data = Constants.EMAIL_USER + ":" + Constants.PASSWORD_USER;
+            String auth = Base64.getEncoder().encodeToString(data.getBytes());
+            headers.put("Authorization", auth);
+            headers.put("db", Constants.DB);
+
+            // endpoint call
+            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "GET", null, null, headers);
+
+            // assertions
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
+
+            // authToken to be used on other unit tests
+            authToken = response.body();
+
+        } catch (Exception e) {
+            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
+        }
+    }
+
+    @Test
+    @Order(2)
+    void shouldRegisterAuthentication() {
+        try {
+            // check for necessary data before continue
+            assumeTrue(authToken != null, "Skipping shouldRegisterAuthentication test because shouldAuthenticateAndGenerateDBToken test did not run.");
+
+            // url
+            String baseUrl = AssimblyGatewayHeadlessContainer.getBaseUrl();
+            String url = String.format("%s/api/authentication/register", baseUrl);
+
+            // headers
+            HashMap<String, String> headers = new HashMap();
+            headers.put("Content-type", MediaType.APPLICATION_JSON_VALUE);
+            headers.put("db", Constants.DB);
+            headers.put("domainName", Constants.DOMAIN_NAME);
+            headers.put("Authorization", authToken);
+
+            // endpoint call
+            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "GET", null, null, headers);
+
+            // assertions
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
+            assertThat(response.headers().map()).containsKey("location");
+
+            // totpSecret to be used on other unit tests
+            String location = response.headers().map().get("location").get(0);
+            location = URLDecoder.decode(location, "UTF-8");
+            totpSecret = HttpUtil.extractSecret(location);
+
+        } catch (Exception e) {
+            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
+        }
+    }
+
+    @Test
+    @Order(3)
+    void shouldValidateAuthentication() {
+        try {
+            // check for necessary data before continue
+            assumeTrue(totpSecret != null, "Skipping shouldValidateAuthentication test because shouldRegisterAuthentication test did not run.");
+
+            // url
+            String baseUrl = AssimblyGatewayHeadlessContainer.getBaseUrl();
+            String url = String.format("%s/api/authentication/validate", baseUrl);
+
+            // headers
+            HashMap<String, String> headers = new HashMap();
+            headers.put("Content-type", MediaType.APPLICATION_JSON_VALUE);
+
+            // body
+            Document bodyDoc = new Document();
+            bodyDoc.append("email", Constants.EMAIL_USER);
+            bodyDoc.append("token", GoogleTOTPUtil.generateToken(totpSecret));
+
+            // endpoint call
+            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "POST", bodyDoc.toJson(), null, headers);
+
+            // assertions
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
+            assertThat(response.body()).isEqualTo("true");
+
+        } catch (Exception e) {
+            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
+        }
+    }
+
+    @Test
+    @Order(4)
+    void shouldRemoveAuthentication() {
+        try {
+            // check for necessary data before continue
+            assumeTrue(authToken != null, "Skipping shouldRemoveAuthentication test because shouldAuthenticateAndGenerateDBToken test did not run.");
+
+            // url
+            String baseUrl = AssimblyGatewayHeadlessContainer.getBaseUrl();
+            String url = String.format("%s/api/authentication/remove", baseUrl);
+
+            // headers
+            HashMap<String, String> headers = new HashMap();
+            headers.put("Content-type", MediaType.APPLICATION_JSON_VALUE);
+
+            // body
+            Document bodyDoc = new Document();
+            headers.put("Authorization", authToken);
+
+            // endpoint call
+            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "DELETE", null, null, headers);
+
+            // assertions
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
 
         } catch (Exception e) {
             fail("Test failed due to unexpected exception: " + e.getMessage(), e);
