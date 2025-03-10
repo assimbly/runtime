785bbf461971d48d749c66eedf967c9a05c33328
Bruno Gonçalves <brunovg@gmail.com>
2025-03-10 16:33:18 +0000

    messages count endpoint unit test

--- brokerRest/src/test/java/org/assimbly/brokerrest/BrokerRuntimeTest.java
+++ brokerRest/src/test/java/org/assimbly/brokerrest/BrokerRuntimeTest.java
@@ -11,6 +11,7 @@ import org.springframework.http.MediaType;
 import java.net.http.HttpResponse;
 import java.util.HashMap;
 
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 
@@ -44,6 +45,38 @@ public class BrokerRuntimeTest {
             assertThat(responseJson.get("totalNumberOfTemporaryQueues").isInt()).isTrue();
             assertThat(responseJson.get("maxConnections").isInt()).isTrue();
             assertThat(responseJson.get("tmpPercentUsage").isInt()).isTrue();
+
+        } catch (Exception e) {
+            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
+        }
+    }
+
+    @Test
+    void shouldGetMessagesCount() {
+        try {
+            // url
+            String baseUrl = AssimblyGatewayBrokerContainer.getBaseUrl();
+            String url = String.format("%s/api/brokers/%s/messages/count", baseUrl, "classic");
+
+            // headers
+            HashMap<String, String> headers = new HashMap();
+            headers.put("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+            // body
+            String body = "67921474ecaafe0007000000,67921474ecaafe0007000000_BottomCenter";
+
+            // endpoint call - check if backend is started
+            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "POST", body, null, headers);
+
+            // asserts
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode responseJson = objectMapper.readTree(response.body());
+
+            assertThat(responseJson.get("details").asText()).isEqualTo("successful");
+            assertThat(responseJson.get("message").asInt()).isEqualTo(0);
+            assertThat(responseJson.get("status").asInt()).isEqualTo(200);
 
         } catch (Exception e) {
             fail("Test failed due to unexpected exception: " + e.getMessage(), e);
