6f0651a821387dfa593544410c3f536cbeed663d
Raymond Meester <raymondmeester@gmail.com>
2025-03-10 17:04:41 +0100

    update to Camel 4.10.2

--- bin/win/build.bat
+++ bin/win/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
 IF "%~1"=="" GOTO :BUILDALL
-mvn -f ..\..\%~1\pom.xml clean install -DskipTests
+mvnd -f ..\..\%~1\pom.xml clean install -DskipTests
 :BUILDALL
-mvn -f ..\..\pom.xml clean install -DskipTests
+mvnd -f ..\..\pom.xml clean install -DskipTests
