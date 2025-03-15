7cda0403227ae2273b45d8d2d058d3cb815ac935
Raymond Meester <raymondmeester@gmail.com>
2025-03-15 15:44:42 +0100

    Update buildandtest.bat

--- bin/win/buildandtest.bat
+++ bin/win/buildandtest.bat
@@ -1,6 +1,6 @@
 @echo off
 
 IF "%~1"=="" GOTO :BUILDALL
-mvn -f ..\..\%~1\pom.xml clean install
+mvnd -f ..\..\%~1\pom.xml clean install
 :BUILDALL
-mvn -f ..\..\pom.xml clean install
+mvnd -f ..\..\pom.xml clean install
