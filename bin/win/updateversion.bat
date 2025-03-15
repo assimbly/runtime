4aed6eb0e42714891c4e6ee5dd99efdd9e89ef3b
Raymond Meester <raymondmeester@gmail.com>
2025-03-15 15:45:15 +0100

    Update updateversion.bat

--- bin/win/updateversion.bat
+++ bin/win/updateversion.bat
@@ -4,8 +4,8 @@ if [%1]==[] goto usage
 echo:
 @echo update version to %1
 echo:
-mvn -f ..\..\pom.xml versions:set -DgenerateBackupPoms=false -DnewVersion=%1
-mvn -f ..\..\pom.xml versions:set-property -Dproperty=assimbly.version -DnewVersion=%1
+mvnd -f ..\..\pom.xml versions:set -DgenerateBackupPoms=false -DnewVersion=%1
+mvnd -f ..\..\pom.xml versions:set-property -Dproperty=assimbly.version -DnewVersion=%1
 :usage
 echo:
 @echo updateversion ^<versionnumber^>
