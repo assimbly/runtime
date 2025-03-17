6b113efae49b9e103e5adfe98eb209e181b2ea00
Bruno Gonçalves <brunovg@gmail.com>
2025-03-17 18:44:13 +0000

    uninstall flow endpoint unit test

--- integrationRest/src/test/java/org/assimbly/integrationrest/IntegrationRuntimeTest.java
+++ integrationRest/src/test/java/org/assimbly/integrationrest/IntegrationRuntimeTest.java
@@ -289,6 +289,42 @@ public class IntegrationRuntimeTest {
     }
 
     @Test
+    @Order(13)
+    void shouldUninstallFlow() {
+        try {
+            assumeTrue(inboundHttpsFlowInstalled, "Skipping shouldUninstallFlow test because shouldInstallFlow test did not run.");
+
+            // url
+            String baseUrl = AssimblyGatewayHeadlessContainer.getBaseUrl();
+            String url = String.format("%s/api/integration/flow/%s/uninstall", baseUrl, inboundHttpsCamelContextProp.get(TestApplicationContext.CamelContextField.id.name()));
+
+            // headers
+            HashMap<String, String> headers = new HashMap();
+            headers.put("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+            // endpoint call - resume flow
+            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "DELETE", null, null, headers);
+
+            // asserts
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode responseJson = objectMapper.readTree(response.body());
+            JsonNode flowJson = responseJson.get("flow");
+
+            assertThat(flowJson.get("name").asText()).isEqualTo(inboundHttpsCamelContextProp.get(TestApplicationContext.CamelContextField.id.name()));
+            assertThat(flowJson.get("id").asText()).isEqualTo(inboundHttpsCamelContextProp.get(TestApplicationContext.CamelContextField.id.name()));
+            assertThat(flowJson.get("time").asText()).matches("\\d+ milliseconds");
+            assertThat(flowJson.get("event").asText()).isEqualTo("stop");
+            assertThat(flowJson.get("message").asText()).isEqualTo("Stopped flow successfully");
+            assertThat(flowJson.get("version").asText()).matches("\\d+");
+
+        } catch (Exception e) {
+            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
+        }
+    }
+
+    @Test
     @Tag("NeedsSchedulerFlowInstalled")
     @Order(20)
     void shouldGetSchedulerFlowInfo() {
