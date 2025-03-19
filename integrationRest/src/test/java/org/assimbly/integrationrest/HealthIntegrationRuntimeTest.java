8a4d881dfff6e67dda0c6579b4641d66d7e9f016
Bruno Gonçalves <brunovg@gmail.com>
2025-03-19 11:47:13 +0000

    health backend flows endpoint unit test

--- integrationRest/src/test/java/org/assimbly/integrationrest/HealthIntegrationRuntimeTest.java
+++ integrationRest/src/test/java/org/assimbly/integrationrest/HealthIntegrationRuntimeTest.java
@@ -55,4 +55,46 @@ public class HealthIntegrationRuntimeTest {
         }
     }
 
+    @Test
+    void shouldGetBackendFlowsInfo() {
+        try {
+            // url
+            String baseUrl = AssimblyGatewayHeadlessContainer.getBaseUrl();
+            String url = String.format("%s/health/backend/flows", baseUrl);
+
+            // headers
+            HashMap<String, String> headers = new HashMap();
+            headers.put("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+            // endpoint call - install flow
+            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "GET", null, null, headers);
+
+            // asserts
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode responseJson = objectMapper.readTree(response.body());
+
+            assertThat(responseJson.get("uptimeMillis").asInt()).isGreaterThan(0);
+            assertThat(responseJson.get("startedSteps").asInt()).isGreaterThanOrEqualTo(0);
+            assertThat(responseJson.get("memoryUsage").asDouble()).isGreaterThan(0);
+            assertThat(responseJson.get("exchangesInflight").asInt()).isGreaterThanOrEqualTo(0);
+            assertThat(responseJson.get("camelVersion").asText()).isNotEmpty();
+            assertThat(responseJson.get("exchangesCompleted").asInt()).isGreaterThanOrEqualTo(0);
+            assertThat(responseJson.get("camelId").asText()).isNotEmpty();
+            assertThat(responseJson.get("uptime").asText()).isNotEmpty();
+            assertThat(responseJson.get("startedFlows").asText()).isNotEmpty();
+            assertThat(responseJson.get("totalThreads").asInt()).isGreaterThan(0);
+            assertThat(responseJson.get("cpuLoadLastMinute").asText()).isNotEmpty();
+            assertThat(responseJson.get("cpuLoadLast15Minutes").asText()).isNotEmpty();
+            assertThat(responseJson.get("exchangesTotal").asInt()).isGreaterThanOrEqualTo(0);
+            assertThat(responseJson.get("exchangesFailed").asInt()).isGreaterThanOrEqualTo(0);
+            assertThat(responseJson.get("cpuLoadLast5Minutes").asText()).isNotEmpty();
+            assertThat(responseJson.get("status").asText()).isEqualTo("Started");
+
+        } catch (Exception e) {
+            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
+        }
+    }
+
 }
