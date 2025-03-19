ffa2f693fda911806a43bc9b9b2d91183fa9cee7
Bruno Gonçalves <brunovg@gmail.com>
2025-03-19 14:37:13 +0000

    install flow by file endpoint unit test

--- integrationRest/src/test/java/org/assimbly/integrationrest/FlowManagerRuntimeTest.java
+++ integrationRest/src/test/java/org/assimbly/integrationrest/FlowManagerRuntimeTest.java
@@ -547,4 +547,36 @@ public class FlowManagerRuntimeTest {
         }
     }
 
+
+    @Test
+    @Order(40)
+    void shouldInstallFlowByFile() {
+        try {
+            // url
+            String baseUrl = AssimblyGatewayHeadlessContainer.getBaseUrl();
+            String url = String.format("%s/api/integration/flow/%s/install/file", baseUrl, inboundHttpsCamelContextProp.get(TestApplicationContext.CamelContextField.id.name()));
+
+            // headers
+            HashMap<String, String> headers = new HashMap();
+            headers.put("Accept", MediaType.APPLICATION_JSON_VALUE);
+            headers.put("Content-type", MediaType.APPLICATION_XML_VALUE);
+
+            // endpoint call - install flow
+            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "POST", (String) inboundHttpsCamelContextProp.get(TestApplicationContext.CamelContextField.camelContext.name()), null, headers);
+
+            // asserts
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode responseJson = objectMapper.readTree(response.body());
+
+            assertThat(responseJson.get("details").asText()).isEqualTo("successful");
+            assertThat(responseJson.get("message").asText()).matches(String.format("flow %s saved in the deploy directory", inboundHttpsCamelContextProp.get(TestApplicationContext.CamelContextField.id.name())));
+            assertThat(responseJson.get("status").asInt()).isEqualTo(200);
+
+        } catch (Exception e) {
+            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
+        }
+    }
+
 }
