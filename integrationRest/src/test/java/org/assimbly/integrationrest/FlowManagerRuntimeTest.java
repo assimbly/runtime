c9f750394854e15e30bce2036c8fad4031bc942b
Bruno Gonçalves <brunovg@gmail.com>
2025-03-19 13:56:44 +0000

    restart flow endpoint unit test

--- integrationRest/src/test/java/org/assimbly/integrationrest/FlowManagerRuntimeTest.java
+++ integrationRest/src/test/java/org/assimbly/integrationrest/FlowManagerRuntimeTest.java
@@ -299,6 +299,48 @@ public class FlowManagerRuntimeTest {
 
     @Test
     @Order(22)
+    void shouldRestartFlow() {
+        try {
+            assumeTrue(inboundHttpsFlowInstalled, "Skipping shouldRestartFlow test because shouldInstallFlow test did not run.");
+
+            // url
+            String baseUrl = AssimblyGatewayHeadlessContainer.getBaseUrl();
+            String url = String.format("%s/api/integration/flow/%s/restart", baseUrl, inboundHttpsCamelContextProp.get(TestApplicationContext.CamelContextField.id.name()));
+
+            // headers
+            HashMap<String, String> headers = new HashMap();
+            headers.put("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+            // endpoint call - resume flow
+            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "GET", null, null, headers);
+
+            // asserts
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode responseJson = objectMapper.readTree(response.body());
+            JsonNode flowJson = responseJson.get("flow");
+
+            JsonNode stepsLoadedJson = flowJson.get("stepsLoaded");
+            assertThat(stepsLoadedJson.get("total").asInt()).isEqualTo(5);
+            assertThat(stepsLoadedJson.get("successfully").asInt()).isEqualTo(5);
+            assertThat(stepsLoadedJson.get("failed").asInt()).isEqualTo(0);
+
+            assertThat(flowJson.get("steps").size()).isEqualTo(5);
+            assertThat(flowJson.get("name").asText()).isEqualTo(inboundHttpsCamelContextProp.get(TestApplicationContext.CamelContextField.id.name()));
+            assertThat(flowJson.get("id").asText()).isEqualTo(inboundHttpsCamelContextProp.get(TestApplicationContext.CamelContextField.id.name()));
+            assertThat(flowJson.get("time").asText()).matches("\\d+ milliseconds");
+            assertThat(flowJson.get("event").asText()).isEqualTo("start");
+            assertThat(flowJson.get("message").asText()).isEqualTo("Started flow successfully");
+            assertThat(flowJson.get("steps").size()).isEqualTo(5);
+
+        } catch (Exception e) {
+            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
+        }
+    }
+
+    @Test
+    @Order(22)
     void shouldUninstallFlow() {
         try {
             assumeTrue(inboundHttpsFlowInstalled, "Skipping shouldUninstallFlow test because shouldInstallFlow test did not run.");
