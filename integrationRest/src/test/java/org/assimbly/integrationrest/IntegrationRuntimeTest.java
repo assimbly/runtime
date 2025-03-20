47205d474e87514866fe1fd5526edfe13e673ebb
Bruno Gonçalves <brunovg@gmail.com>
2025-03-20 15:29:30 +0000

    last error endpoint unit test

--- integrationRest/src/test/java/org/assimbly/integrationrest/IntegrationRuntimeTest.java
+++ integrationRest/src/test/java/org/assimbly/integrationrest/IntegrationRuntimeTest.java
@@ -305,6 +305,38 @@ class IntegrationRuntimeTest {
     }
 
     @Test
+    void shouldGetLastError() {
+        try {
+            // url
+            String baseUrl = AssimblyGatewayHeadlessContainer.getBaseUrl();
+            String url = String.format("%s/api/integration/lasterror", baseUrl);
+
+            // headers
+            HashMap<String, String> headers = new HashMap();
+            headers.put("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+            // endpoint call - get list of flows
+            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "GET", null, null, headers);
+
+            // asserts
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode responseJson = objectMapper.readTree(response.body());
+
+            assertThat(responseJson.get("details").asText()).isEqualTo("successful");
+            assertThat(responseJson.get("message").asText()).isNotEmpty();
+            assertThat(responseJson.get("status").asInt()).isEqualTo(200);
+            assertThat(responseJson.get("timestamp").asText()).isNotEmpty();
+            boolean isValid = Utils.isValidDate(responseJson.get("timestamp").asText(), "yyyy-MM-dd HH:mm:ss.SSS");
+            assertThat(isValid).as("Check if timestamp is a valid date").isTrue();
+
+        } catch (Exception e) {
+            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
+        }
+    }
+
+    @Test
     void shouldBeStarted() {
         try {
             // url
