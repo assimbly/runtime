fb8c4fadc6e350c7f2c6e475954ab6e3d0ffe171
Bruno Gonçalves <brunovg@gmail.com>
2025-03-27 13:58:27 +0000

    TopicManagerRuntime unit tests

---
+++ brokerRest/src/test/java/org/assimbly/brokerrest/TopicManagerRuntimeTest.java
@@ -0,0 +1,289 @@
+package org.assimbly.brokerrest;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assimbly.brokerrest.testcontainers.AssimblyGatewayBrokerContainer;
+import org.assimbly.brokerrest.utils.HttpUtil;
+import org.assimbly.brokerrest.utils.Utils;
+import org.eclipse.jetty.http.HttpStatus;
+import org.junit.jupiter.api.*;
+import org.springframework.http.MediaType;
+
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class TopicManagerRuntimeTest {
+
+    private static final String BROKER_TYPE = "classic";
+
+    private static final String TOPIC_TEST = "topic_test";
+
+    private static boolean topicCreated = false;
+    private static boolean messageSentToTopic = false;
+
+    private static AssimblyGatewayBrokerContainer container;
+
+    @BeforeAll
+    static void init() {
+        container = new AssimblyGatewayBrokerContainer();
+        container.init();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        container.stop();
+    }
+
+    @BeforeEach
+    void setUp(TestInfo testInfo) {
+        try {
+
+            if (testInfo.getTags().contains("NeedsMessageOnTopic") && !messageSentToTopic) {
+                // url
+                String baseUrl = container.getBrokerBaseUrl();
+                String url = String.format("%s/api/brokers/%s/message/%s/send", baseUrl, BROKER_TYPE, TOPIC_TEST);
+
+                // params
+                HashMap<String, String> params = new HashMap();
+                params.put("messageHeaders", "{\"test\":\"1234\"}");
+
+                // headers
+                HashMap<String, String> headers = new HashMap();
+                headers.put("Accept", MediaType.APPLICATION_JSON_VALUE);
+                headers.put("Content-type", MediaType.TEXT_PLAIN_VALUE);
+
+                // endpoint call
+                HttpUtil.makeHttpCall(url, "POST", "Hello World!", params, headers);
+
+                messageSentToTopic = true;
+            }
+
+        } catch (Exception e) {
+            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
+        }
+    }
+
+    @Test
+    @Order(1)
+    void shouldCreateTopic() {
+        try {
+            // url
+            String baseUrl = container.getBrokerBaseUrl();
+            String url = String.format("%s/api/brokers/%s/topic/%s", baseUrl, BROKER_TYPE, TOPIC_TEST);
+
+            // headers
+            HashMap<String, String> headers = new HashMap();
+            headers.put("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+            // endpoint call
+            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "POST", null, null, headers);
+
+            // asserts
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode responseJson = objectMapper.readTree(response.body());
+
+            assertThat(responseJson.get("details").asText()).isEqualTo("successful");
+            assertThat(responseJson.get("message").asText()).isEqualTo("success");
+            assertThat(responseJson.get("status").asInt()).isEqualTo(200);
+            assertThat(responseJson.get("timestamp").asText()).isNotEmpty();
+            boolean isValid = Utils.isValidDate(responseJson.get("timestamp").asText(), "yyyy-MM-dd HH:mm:ss.SSS");
+            assertThat(isValid).as("Check if timestamp is a valid date").isTrue();
+
+            topicCreated = true;
+
+        } catch (Exception e) {
+            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
+        }
+    }
+
+    @Test
+    @Order(2)
+    void shouldGetTopic() {
+        try {
+            // check for necessary data before continue
+            assumeTrue(topicCreated, "Skipping shouldGetTopic test because shouldCreateTopic test did not run.");
+
+            // url
+            String baseUrl = container.getBrokerBaseUrl();
+            String url = String.format("%s/api/brokers/%s/topic/%s", baseUrl, BROKER_TYPE, TOPIC_TEST);
+
+            // headers
+            HashMap<String, String> headers = new HashMap();
+            headers.put("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+            // endpoint call
+            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "GET", null, null, headers);
+
+            // asserts
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode responseJson = objectMapper.readTree(response.body());
+
+            JsonNode topicJson = responseJson.get("topic");
+            assertThat(topicJson.get("temporary").asText()).isEqualTo("false");
+            assertThat(topicJson.get("address").asText()).isEqualTo(TOPIC_TEST);
+            assertThat(topicJson.get("numberOfConsumers").asInt()).isZero();
+            assertThat(topicJson.get("name").asText()).isEqualTo(TOPIC_TEST);
+            assertThat(topicJson.get("numberOfMessages").asInt()).isZero();
+
+        } catch (Exception e) {
+            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
+        }
+    }
+
+    @Test
+    @Order(2)
+    void shouldGetTopics() {
+        try {
+            // check for necessary data before continue
+            assumeTrue(topicCreated, "Skipping shouldGetTopics test because shouldCreateTopic test did not run.");
+
+            // url
+            String baseUrl = container.getBrokerBaseUrl();
+            String url = String.format("%s/api/brokers/%s/topics", baseUrl, BROKER_TYPE);
+
+            // headers
+            HashMap<String, String> headers = new HashMap();
+            headers.put("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+            // endpoint call
+            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "GET", null, null, headers);
+
+            // asserts
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode responseJson = objectMapper.readTree(response.body());
+
+            JsonNode topicsJson = responseJson.get("topics").get("topic");
+            assertThat(topicsJson.isArray()).isTrue();
+            assertThat(topicsJson.size()).isPositive();
+
+            JsonNode topicJson = topicsJson.get(0);
+            assertThat(topicJson.get("temporary").asText()).isEqualTo("false");
+            assertThat(topicJson.get("address").asText()).isNotNull();
+            assertThat(topicJson.get("numberOfConsumers").asInt()).isZero();
+            assertThat(topicJson.get("name").asText()).isNotNull();
+            assertThat(topicJson.get("numberOfMessages").asInt()).isZero();
+
+        } catch (Exception e) {
+            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
+        }
+    }
+
+    @Test
+    @Order(10)
+    void shouldClearTopic() {
+        try {
+            // check for necessary data before continue
+            assumeTrue(topicCreated, "Skipping shouldClearTopic test because shouldCreateTopic test did not run.");
+
+            // url
+            String baseUrl = container.getBrokerBaseUrl();
+            String url = String.format("%s/api/brokers/%s/topic/%s/clear", baseUrl, BROKER_TYPE, TOPIC_TEST);
+
+            // headers
+            HashMap<String, String> headers = new HashMap();
+            headers.put("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+            // endpoint call
+            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "POST", null, null, headers);
+
+            // asserts
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode responseJson = objectMapper.readTree(response.body());
+
+            assertThat(responseJson.get("details").asText()).isEqualTo("successful");
+            assertThat(responseJson.get("message").asText()).isEqualTo("success");
+            assertThat(responseJson.get("status").asInt()).isEqualTo(200);
+            assertThat(responseJson.get("timestamp").asText()).isNotEmpty();
+            boolean isValid = Utils.isValidDate(responseJson.get("timestamp").asText(), "yyyy-MM-dd HH:mm:ss.SSS");
+            assertThat(isValid).as("Check if timestamp is a valid date").isTrue();
+
+        } catch (Exception e) {
+            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
+        }
+    }
+
+    @Test
+    @Order(10)
+    void shouldClearTopics() {
+        try {
+            // check for necessary data before continue
+            assumeTrue(topicCreated, "Skipping shouldClearTopics test because shouldCreateTopic test did not run.");
+
+            // url
+            String baseUrl = container.getBrokerBaseUrl();
+            String url = String.format("%s/api/brokers/%s/topics/clear", baseUrl, BROKER_TYPE);
+
+            // headers
+            HashMap<String, String> headers = new HashMap();
+            headers.put("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+            // endpoint call
+            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "POST", null, null, headers);
+
+            // asserts
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode responseJson = objectMapper.readTree(response.body());
+
+            assertThat(responseJson.get("details").asText()).isEqualTo("successful");
+            assertThat(responseJson.get("message").asText()).isEqualTo("success");
+            assertThat(responseJson.get("status").asInt()).isEqualTo(200);
+            assertThat(responseJson.get("timestamp").asText()).isNotEmpty();
+            boolean isValid = Utils.isValidDate(responseJson.get("timestamp").asText(), "yyyy-MM-dd HH:mm:ss.SSS");
+            assertThat(isValid).as("Check if timestamp is a valid date").isTrue();
+
+        } catch (Exception e) {
+            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
+        }
+    }
+
+    @Test
+    @Order(20)
+    void shouldDeleteTopic() {
+        try {
+            // check for necessary data before continue
+            assumeTrue(topicCreated, "Skipping shouldDeleteTopic test because shouldCreateTopic test did not run.");
+
+            // url
+            String baseUrl = container.getBrokerBaseUrl();
+            String url = String.format("%s/api/brokers/%s/topic/%s", baseUrl, BROKER_TYPE, TOPIC_TEST);
+
+            // headers
+            HashMap<String, String> headers = new HashMap();
+            headers.put("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+            // endpoint call
+            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "DELETE", null, null, headers);
+
+            // asserts
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode responseJson = objectMapper.readTree(response.body());
+
+            assertThat(responseJson.get("details").asText()).isEqualTo("successful");
+            assertThat(responseJson.get("status").asInt()).isEqualTo(200);
+            assertThat(responseJson.get("timestamp").asText()).isNotEmpty();
+            boolean isValid = Utils.isValidDate(responseJson.get("timestamp").asText(), "yyyy-MM-dd HH:mm:ss.SSS");
+            assertThat(isValid).as("Check if timestamp is a valid date").isTrue();
+
+        } catch (Exception e) {
+            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
+        }
+    }
+
+}
