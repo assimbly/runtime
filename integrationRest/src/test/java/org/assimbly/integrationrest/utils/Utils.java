fa44cb1305d31d7883c038612a7e69b49aeaffb6
Bruno Gonçalves <brunovg@gmail.com>
2025-03-28 16:59:36 +0000

    extractRouteFromXmlByRouteId

--- integrationRest/src/test/java/org/assimbly/integrationrest/utils/Utils.java
+++ integrationRest/src/test/java/org/assimbly/integrationrest/utils/Utils.java
@@ -2,8 +2,24 @@ package org.assimbly.integrationrest.utils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
 import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -65,5 +81,54 @@ public class Utils {
             log.error(String.format("Error to load %s file from resources", fileName), e);
             return null;
         }
+    }
+
+    public static String extractRouteFromXmlByRouteId(String xml, String routeId) throws Exception {
+
+        // create a DocumentBuilderFactory and set up a DocumentBuilder
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        // parse the string content into a Document
+        InputSource inputSource = new InputSource(new StringReader(xml));
+        Document document = builder.parse(inputSource);
+
+        // create XPath object
+        XPathFactory xPathFactory = XPathFactory.newInstance();
+        XPath xpath = xPathFactory.newXPath();
+
+        // xpath expression to find the route with the specific id
+        String expression = "//route[@id='" + routeId + "']";
+
+        // execute XPath to find the route element
+        NodeList routeNodes = (NodeList) xpath.evaluate(expression, document, XPathConstants.NODESET);
+
+        // check if the route is found
+        if (routeNodes.getLength() > 0) {
+            // get the first matching route node (you can change this to handle multiple matches if needed)
+            Node routeNode = routeNodes.item(0);
+
+            // convert the node to a string (XML)
+            return getStringFromNode(routeNode);
+        }
+        return null;
+    }
+
+    private static String getStringFromNode(Node node) throws Exception {
+        // create a transformer to convert the node to a string
+        StringWriter writer = new StringWriter();
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+
+        // format the output for better readability
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+        // prevent the XML declaration from being included in the output
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+
+        // convert the node to a string
+        transformer.transform(new DOMSource(node), new StreamResult(writer));
+        return writer.toString();
     }
 }
