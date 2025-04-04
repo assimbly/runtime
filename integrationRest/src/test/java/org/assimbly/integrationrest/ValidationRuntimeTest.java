38dd42d6ed833de2dd2a1d889ebad29385bb600c
Cachado <cacha@CACHADO>
2025-04-04 12:44:57 +0100

    ValidationRuntime - Script unit test

--- integrationRest/src/test/java/org/assimbly/integrationrest/ValidationRuntimeTest.java
+++ integrationRest/src/test/java/org/assimbly/integrationrest/ValidationRuntimeTest.java
@@ -122,4 +122,53 @@ class ValidationRuntimeTest {
         }
     }
 
+    @Test
+    void shouldValidateScriptWithSuccess() {
+        try {
+            String baseUrl = container.getBaseUrl();
+            String url = baseUrl + "/api/validation/script";
+
+            // headers
+            HashMap<String, String> headers = new HashMap<>();
+            headers.put("Accept", MediaType.APPLICATION_JSON_VALUE);
+            headers.put("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+
+            // script
+            JSONObject scriptJson = new JSONObject();
+            scriptJson.put("language", "groovy");
+            scriptJson.put("script", "return 1 + 1;");
+
+            // Exchange
+            JSONObject exchangeJson = new JSONObject();
+            exchangeJson.put("body", "");
+
+            // headers
+            JSONObject headersJson = new JSONObject();
+            exchangeJson.put("headers", headersJson);
+
+            //properties
+            JSONObject propertiesJson = new JSONObject();
+            exchangeJson.put("properties", propertiesJson);
+
+            // body
+            JSONObject bodyJson = new JSONObject();
+            bodyJson.put("script", scriptJson);
+            bodyJson.put("exchange", exchangeJson);
+
+            HttpResponse<String> response = HttpUtil.makeHttpCall(url, "POST", bodyJson.toString(), null, headers);
+
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK_200);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode responseJson = objectMapper.readTree(response.body());
+
+            assertThat(responseJson.get("code").asInt()).isEqualTo(1);
+            assertThat(responseJson.get("result").asText()).isEqualTo("2");
+
+        } catch (Exception e) {
+            fail("Test failed due to unexpected exception: " + e.getMessage(), e);
+        }
+    }
+
+
 }
