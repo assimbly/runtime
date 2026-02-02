7cbe4d799240a9c6544adaede92c7f670e328f7f
Bruno Gonçalves <brunovg@gmail.com>
2026-02-02 17:49:55 +0000

    blacklist specific java system classes

--- dil/src/main/java/org/assimbly/dil/validation/ScriptValidator.java
+++ dil/src/main/java/org/assimbly/dil/validation/ScriptValidator.java
@@ -1,5 +1,6 @@
 package org.assimbly.dil.validation;
 
+import groovy.lang.GroovyShell;
 import org.apache.camel.Exchange;
 import org.apache.camel.language.groovy.GroovyExpression;
 //import org.apache.camel.language.js.JavaScriptExpression;
@@ -9,8 +10,12 @@ import org.assimbly.dil.validation.beans.script.EvaluationResponse;
 import org.assimbly.dil.validation.beans.script.ExchangeDto;
 import org.assimbly.dil.validation.beans.script.ScriptDto;
 import org.assimbly.dil.validation.scripts.ExchangeMarshaller;
+import org.codehaus.groovy.control.CompilerConfiguration;
+import org.codehaus.groovy.control.customizers.SecureASTCustomizer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
 
 public class ScriptValidator {
 
@@ -48,14 +53,43 @@ public class ScriptValidator {
 
     private EvaluationResponse validateGroovyScript(ExchangeDto exchangeDto, String script) {
         try {
-            GroovyExpression groovyExpression = new GroovyExpression(script);
+            // 1. Setup the same Sandbox Security
+            SecureASTCustomizer customizer = new SecureASTCustomizer();
+            customizer.setDisallowedReceivers(Arrays.asList(
+                    "java.lang.System", "java.lang.Runtime", "java.util.TimeZone",
+                    "java.util.Locale", "java.lang.Class", "java.lang.ClassLoader",
+                    "java.lang.Thread", "java.lang.ThreadGroup", "java.lang.reflect.Method",
+                    "java.lang.reflect.Field", "java.lang.reflect.Constructor"
+            ));
+
+            CompilerConfiguration config = new CompilerConfiguration();
+            config.addCompilationCustomizers(customizer);
+
+            // 2. Prepare the data
             Exchange exchangeRequest = ExchangeMarshaller.unmarshall(exchangeDto);
-            Object response = groovyExpression.evaluate(exchangeRequest, String.class);
+
+            // 3. Evaluate using the Secure Shell instead of GroovyExpression
+            GroovyShell shell = new GroovyShell(config);
+
+            // Standard bindings for Camel-like scripts
+            shell.setProperty("exchange", exchangeRequest);
+            shell.setProperty("request", exchangeRequest.getIn());
+            shell.setProperty("headers", exchangeRequest.getIn().getHeaders());
+            shell.setProperty("body", exchangeRequest.getIn().getBody());
+
+            Object response = shell.evaluate(script);
+
+            // 4. Finalize response
             ExchangeDto exchangeDtoResponse = ExchangeMarshaller.marshall(exchangeRequest);
             return createOKRequestResponse(exchangeDtoResponse, String.valueOf(response));
+
+        } catch (SecurityException | org.codehaus.groovy.control.CompilationFailedException e) {
+            // This catches script hacks like TimeZone.setDefault() or syntax errors
+            log.error("Sandbox Security Violation or Syntax Error: ", e);
+            return createBadRequestResponse(exchangeDto, "Security/Validation Error: " + e.getMessage());
         } catch (Exception e) {
-            log.error("Invalid groovy script: '", e);
-            return createBadRequestResponse(exchangeDto, "Invalid groovy script: '" + e.getMessage() + "'");
+            log.error("Execution error: ", e);
+            return createBadRequestResponse(exchangeDto, "Execution Error: " + e.getMessage());
         }
     }
 
