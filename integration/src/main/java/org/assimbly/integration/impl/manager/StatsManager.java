8a40473439f479204c3a1f49681bd60e0d206831
Bruno Gonçalves <brunovg@gmail.com>
2026-06-09 15:16:22 +0100

    removed debug logs

--- integration/src/main/java/org/assimbly/integration/impl/manager/StatsManager.java
+++ integration/src/main/java/org/assimbly/integration/impl/manager/StatsManager.java
@@ -25,8 +25,6 @@ import org.assimbly.dil.event.collect.MicrometerTimestampRoutePolicyFactory;
 import org.assimbly.docconverter.DocConverter;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.openjdk.jol.info.ClassLayout;
-import org.openjdk.jol.info.GraphLayout;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -153,17 +151,7 @@ public class StatsManager {
 
         List<Route> routes = context.getRoutesByGroup(flowId);
 
-        System.setProperty("jol.magicFieldOffset", "true");
-
         for (Route route : routes) {
-
-            // 1. SHALLOW SIZE: Just the Route object container itself
-            long shallowSize = ClassLayout.parseInstance(route).instanceSize();
-            System.out.println("Shallow Size: " + shallowSize + " bytes");
-
-            System.out.println(GraphLayout.parseInstance(route).toFootprint());
-
-            System.out.println(GraphLayout.parseInstance(route).toPrintable());
 
             ManagedRouteMBean managedRoute = managedContext.getManagedRoute(route.getId());
 
