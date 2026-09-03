743ea0384edbc5da4054b7dc8878d46cc5b03e6e
Bruno Gonçalves <brunovg@gmail.com>
2026-09-03 13:29:18 +0100

    revert changes done for issue #715 - ai json schema

--- dil/src/main/java/org/assimbly/dil/transpiler/marshalling/core/RouteTemplate.java
+++ dil/src/main/java/org/assimbly/dil/transpiler/marshalling/core/RouteTemplate.java
@@ -607,11 +607,6 @@ public class RouteTemplate {
 
         Element param = doc.createElementNS("http://camel.apache.org/schema/spring","parameter");
         param.setAttribute("name", name);
-
-        if (value != null && (value.contains("{") || value.contains("}")) && !value.startsWith("BASE64:")) {
-            value = "BASE64:" + java.util.Base64.getEncoder().encodeToString(value.getBytes(java.nio.charset.StandardCharsets.UTF_8));
-        }
-
         param.setAttribute("value", value);
 
         return param;
